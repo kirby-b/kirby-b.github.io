edd17de6007b02341a200d9a0f401bf07e7b796b
kirby-b <kirby.brodie66@gmail.com>
2023-06-08 10:26:48 -0400

    added more code
    -beginning of functions for calculating
    -added style sheet (to be changed)
    -calls to functions
    -other little changes

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,16 +1,40 @@
-//Make an array of substrings using split to get numbers and operators
-function addFunc(){
+var numOne = parseInt(document.getElementsByClassName("numOne"));
+var numTwo = parseInt(document.getElementsByClassName("numTwo"));
 
+function calculate(){
+    if(document.getElementsByClassName("sign") == "+"){
+        addFunc();
+    }
+    else if(document.getElementsByClassName("sign") == "-"){
+        subtractFunc();
+    }
+    else if(document.getElementsByClassName("sign") == "/"){
+        divideFunc();
+    }
+    else if(document.getElementsByClassName("sign") == "*"){
+        multiplyFunc();
+    }
+    else if(document.getElementsByClassName("sign") == "%"){
+        moduloFunc();
+    }
+}
+function addFunc(){
+    var result = numOne + numTwo;
+    document.getElementById('result').innerHTML = result;
 }
 function subtractFunc(){
-    
-}
-function multiplyFunc(){
-    
+    var result = numOne - numTwo;
+    document.getElementById('result').innerHTML = result;
 }
 function divideFunc(){
-    
+    var result = numOne / numTwo;
+    document.getElementById('result').innerHTML = result;
+}
+function multiplyFunc(){
+    var result = numOne * numTwo;
+    document.getElementById('result').innerHTML = result;
 }
 function moduloFunc(){
-    
+    var result = numOne % numTwo;
+    document.getElementById('result').innerHTML = result;
 }
--- rand-num/random.js
+++ rand-num/random.js
@@ -1,6 +1,6 @@
 function randFunc() {
-	var min = parseInt(document.getElementsByClassName('min')[0].value);
-	var max = parseInt(document.getElementsByClassName('max')[0].value);
+	var min = parseInt(document.getElementsByClassName("min")[0].value);
+	var max = parseInt(document.getElementsByClassName("max")[0].value);
 
 	var result = Math.floor(Math.random() * (max - min + 1)) + min;
 
