ed3500dd5d903252367b6641cc141e7d0b7ab9c7
kirby-b <kirby.brodie66@gmail.com>
2023-06-22 18:16:17 -0400

    Update functions.js

--- Password-Generator/functions.js
+++ Password-Generator/functions.js
@@ -1,12 +1,13 @@
 function passgen(){
     var result = "";
+    var passwordParts = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%&?";
     /*Make it generate a 12 character password that contains random letters, numbers, and characters
     Pseudocode:
     for 12 times{
         grab a character from a split string/list
         add it to a string
         check to make sure there are no immediate repeats
-        make sure it contains at least 3 letters, 3 numbers, and 3 special characters
+        make sure it is at least 1/3 letters, 1/3 numbers, and 1/3 special characters
     }*/
     document.getElementById("result").innerHTML = result;
 }
