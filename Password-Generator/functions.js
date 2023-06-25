8f233dddaa0dc414f5c514b617fcc2edbea87edf
kirby-b <kirby.brodie66@gmail.com>
2023-06-25 17:00:15 -0400

    work in progress
    -added checking
    -added whiles
    -should generate a password to be checked
    - working on checking more of the password

--- Password-Generator/functions.js
+++ Password-Generator/functions.js
@@ -1,19 +1,31 @@
-var result = "";
+var result;
 var passwordParts = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%&?";
 const partsArray = passwordParts.split("");
 function passgen(){
+    result = "";
     /*Make it generate a 12 character password that contains random letters, numbers, and characters
-    Pseudocode:
-    call get parts
-    check to make sure there are no immediate repeats
-    make sure it is at least 1/3 letters, 1/3 numbers, and 1/3 special characters
-    */
+    call getparts()*/
+    var i = 0;
+    while (i < result.length){
+        /*check to make sure there are no immediate repeats
+        make sure it is at least 1/3 letters, 1/3 numbers, and 1/3 special characters */
+        const letterPattern = /[a-z]/i;
+        const numPattern = /[0-9]/i;
+        if (letterPattern.test(result[i]) == True || numPattern.test(result[i]) == True){
+            if(result[i] == result[i + 1]){
+                passgen()
+            }
+        }
+    }
     document.getElementById("result").innerHTML = result;
 }
 function getparts(){
     var i = 0;
+    var selected = "";
     while (i < 12){
         /*grab a character from a split string/list
         add it to a string*/
+        selected = partsArray[Math.floor(Math.random() * passwordParts.length)];
+        result += selected;
     }
 }
